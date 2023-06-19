74a2bc5bda6a8e9cbb61b7a3bff3ea86d6aedd95
weaver <vesi1974@yandex.ru>
2023-06-19 12:52:03 +0500

    HomeWork0

--- src/main/java/ru/patyukov/Test.java
+++ src/main/java/ru/patyukov/Test.java
@@ -2,6 +2,13 @@ package ru.patyukov;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("\t \t ФОТО ПРОВОДНИК \n");
+        System.out.println("1 показать фото за сегодня");
+        System.out.println("2 показать фото за вчера");
+        System.out.println("3 показать фото за указанную дату");
+        System.out.println("4 показать фото за все время");
+        System.out.println("5 управление фото");
+        System.out.println("6 настройки приложения \n");
+        System.out.println("введите цифру:___");
     }
 }
